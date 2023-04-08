b1dfa90002bc31060cbd35afe2643b97226e7e54
mmsharepoint <markus.moeller.ed@outlook.de>
2023-04-08 21:02:17 +0200

    Initial commit

---
+++ MsgextGraphActCfg/Helpers/AzureHelper.cs
@@ -0,0 +1,22 @@
+﻿using Azure.Data.AppConfiguration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using MsgextGraphSrchCfg.Models;
+
+namespace MsgextGraphSrchCfg.Helpers
+{
+    public class AzureHelper
+    {
+        private readonly ConfigurationClient _client;
+        public AzureHelper(IConfiguration config) 
+        {
+            string _connectionString = config["AZURE_CONFIG_CONNECTION_STRING"];
+            _client = new ConfigurationClient(_connectionString);
+        }
+
+        public void storeConfigValue(string key, string value)
+        {
+            _client.SetConfigurationSetting(key, value);
+        }
+    }
+}
